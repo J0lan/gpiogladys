364f4accc23f13f0d8b7960ac8e18461fecfd766
J0lan <36326575+J0lan@users.noreply.github.com>
2018-03-21 18:50:37 +0100

    Add files via upload

---
+++ gpio/lib/exec.js
@@ -0,0 +1,18 @@
+var Gpio = require('onoff').Gpio;
+var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+
+
+module.exports = function exec(params){
+	switch(params.deviceType.type){
+      case 'binary': 
+        if(params.state.value === 1){
+            LED.writeSync(1);
+        } else {
+            LED.writeSync(0);
+        }  
+      break;
+	}
+	LED.unexport(); // Unexport GPIO and free resources 	
+};
+
+	
